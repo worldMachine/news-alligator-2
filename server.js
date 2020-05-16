7f080249f4009130ba956f7227d07458087760de
SWE-Student <davi212@usc.edu>
2020-05-16 16:14:05 -0700

    added cors

--- server.js
+++ server.js
@@ -2,9 +2,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("./api/routes");
+var cors = require("cors");
 
 const app = express();
 // const port = process.env.PORT || 8080;
+
+app.use(cors());
 
 //connect to database
 require("./api/config/db.config");
