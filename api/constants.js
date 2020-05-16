08ca3ae560ac440be5d215988bc83e0507f480c7
SWE-Student <davi212@usc.edu>
2020-05-15 22:30:04 -0700

    api inherits mono repo

---
+++ api/constants.js
@@ -0,0 +1,5 @@
+module.exports = Object.freeze({
+  // MONGO_CONN_STRING:
+  //   "mongodb+srv://brady:fuLXDMQtb2lJ8kN4@cluster0-1y62m.mongodb.net/test?retryWrites=true&w=majority",
+  MONGO_CONN_STRING: process.env.MONGO_CONN_STRING,
+});
--- news-alligator-rss-scraper/api/routes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-// const userRoutes = require('./user.route');
-
-// router.use('/users', userRoutes);
-
-module.exports = router;
