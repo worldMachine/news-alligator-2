cf98a5299176a125a0a889f10f4b17d59267d74b
SWE-Student <davi212@usc.edu>
2020-05-15 21:41:29 -0700

    ready api for deployment, added rss sources

--- news-alligator-rss-scraper/api/services/scheduler.service.js
+++ news-alligator-rss-scraper/api/services/scheduler.service.js
@@ -1,11 +1,11 @@
 // var schedule = require('node-schedule');
 // https://www.npmjs.com/package/node-schedule
 
-var cron = require('cron');
-var rssParserService = require('./rss-parser.service');
-const htmlService = require('./html.service');
-const articleService = require('./article.service');
-const scraperService = require('./scraper.service');
+var cron = require("cron");
+var rssParserService = require("./rss-parser.service");
+const htmlService = require("./html.service");
+const articleService = require("./article.service");
+const scraperService = require("./scraper.service");
 
 /**
  * @function - cron job to run at specific hour/minute based on recurrence rule - trigger rss parser service
@@ -14,86 +14,103 @@ const scraperService = require('./scraper.service');
  */
 
 // console.log('Before cron job instantiation');
-const job = new cron.CronJob('0 */1 * * * *', async function() {
-	const d = new Date();
-    console.log('Every Fifth Minute:', d);
-    
-    let url = 'https://www.digitaltrends.com/feed/'
-    let url2 = 'http://feeds.arstechnica.com/arstechnica/index'
-    let url3 = 'https://www.theverge.com/rss/index.xml'
-    let url4 = 'https://www.wired.com/feed/rss'
+const job = new cron.CronJob("0 */2 * * * *", async function () {
+  const d = new Date();
+  console.log("Every Fifth Minute:", d);
 
-    
+  let sources = [
+    {
+      name: "Digital Trends",
+      link: "https://www.digitaltrends.com/feed/",
+      pubId: 0,
+    },
+    {
+      name: "Ars Technica",
+      link: "http://feeds.arstechnica.com/arstechnica/index",
+      pubId: 1,
+    },
+    {
+      name: "The Verge",
+      link: "https://www.theverge.com/rss/index.xml",
+      pubId: 2,
+    },
+    { name: "Wired", link: "https://www.wired.com/feed/rss", pubId: 3 },
+    {
+      name: "TechCrunch",
+      link: "http://feeds.feedburner.com/TechCrunch/",
+      pubId: 4,
+    },
+    { name: "CNET", link: "https://www.cnet.com/rss/all/", pubId: 5 },
+    {
+      name: "The Next Web",
+      link: "http://feeds2.feedburner.com/thenextweb",
+      pubId: 6,
+    },
+    // {
+    //   name: "Hacker Noon",
+    //   link: "https://hackernoon.com/feed?ref=hackernoon.com",
+    // },
+  ];
 
-    let feedLinks = [url, url2, url3, url4];
+  //   let url = "https://www.digitaltrends.com/feed/";
+  //   let url2 = "http://feeds.arstechnica.com/arstechnica/index";
+  //   let url3 = "https://www.theverge.com/rss/index.xml";
+  //   let url4 = "https://www.wired.com/feed/rss";
 
-    for (link of feedLinks) {
-        console.log("parsing...")
-        let feed = await rssParserService.parseRssFeed(link);
+  //   let feedLinks = [url, url2, url3, url4];
 
-        for (item of feed.items) {
-            let article = await articleService.formatArticle(item, feed.title);
-
-            let candidateArticle = await articleService.findByGuid(article.guid);
-
-            if (candidateArticle) {
-                console.log("article exists")
-                // console.log(candidateArticle)
-                break;
-            }
-
-            let articleBody = await scraperService.scrapeWebsiteArticle(article.link);
-
-            articleBody = await htmlService.getTextFromHTML(articleBody)
-
-            article.body = articleBody
-
-            // console.log(article.link, article.body);
-
-            // console.log(article.publisher,",",article.title,":",article.link,":",article.imageLink)
-
-            articleService.addArticleJson(article);
-        }
-
-
+  for (source of sources) {
+    let link = source.link;
+    console.log("parsing...", source.name, link);
+    let feed = await rssParserService.parseRssFeed(link);
+    for (item of feed.items) {
+      if (source.pubId == 6) {
+        console.log(item);
+      }
+      let article = await articleService.formatArticle(item, source.name);
+      let candidateArticle = await articleService.findByGuid(article.guid);
+      if (candidateArticle) {
+        console.log("article exists");
+        break;
+      }
+      let articleBody = await scraperService.scrapeWebsiteArticle(article.link);
+      articleBody = await htmlService.getTextFromHTML(articleBody);
+      article.body = articleBody;
+      article.publisher = source.name;
+      article.publisherId = source.pubId;
+      console.log("article: ");
+      console.log(article);
+      articleService.addArticleJson(article);
     }
+  }
 
-    console.log("finished saving articles.")
+  console.log("finished saving articles.");
 
-    // Test New Feeds:
+  // Test New Feeds:
 
-    // let testLink = 'https://www.wired.com/feed/rss'
+  //   let testLink = "http://feeds.feedburner.com/TechCrunch/";
 
-    // let feed = await rssParserService.parseRssFeed(testLink);
+  //   let feed = await rssParserService.parseRssFeed(testLink);
 
-    // console.log("TESTING FEED: ", feed.title)
-    // for (item of feed.items) {
-    //     // console.dir(item)
-    //     // console.log(item.title)
-    //     // console.log(item['media:thumbnail'].$.url)
-    //     // let article = await articleService.formatArticle(item, feed.title);
-    //     // console.log(article)
-        
-    //     // console.dir(item)
-    // }
+  //   console.log("TESTING FEED: ", feed.title);
+  //   for (item of feed.items) {
+  //     // console.dir(item)
+  //     // console.log(item.title)
+  //     // console.log(item['media:thumbnail'].$.url)
+  //     //   let article = await articleService.formatArticle(item, feed.title);
+  //     //   console.log(article)
 
-    // let art = await scraperService.scrapeWebsiteArticle('https://www.wired.com/gallery/best-android-phones/');
+  //     console.dir(item);
+  //   }
 
-    //https://www.digitaltrends.com/dtdeals/best-apple-deals/
+  //   //   let art = await scraperService.scrapeWebsiteArticle('https://www.wired.com/gallery/best-android-phones/');
 
-    // art = await htmlService.getTextFromHTML(art)
+  //   //   https://www.digitaltrends.com/dtdeals/best-apple-deals/
 
-    // console.log(art)
-    // console.log("got it")
+  //   art = await htmlService.getTextFromHTML(art);
 
-
-
-
-
-    
-    
-
-
+  //   console.log(art);
+  //   console.log("got it");
 });
-console.log('Cron Job Instantiated');
+console.log("Cron Job Instantiated");
 job.start();
--- news-alligator-rss-scraper/server.js
+++ news-alligator-rss-scraper/server.js
@@ -1,28 +1,28 @@
 //modules
-const express = require('express');
-const bodyParser = require('body-parser');
-const router = require('./api/routes');
+const express = require("express");
+const bodyParser = require("body-parser");
+const router = require("./api/routes");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 //connect to database
-require('./api/config/db.config');
+require("./api/config/db.config");
 
-require('./api/services/scheduler.service')
+require("./api/services/scheduler.service");
 // require('./api/services/feed-sub.service')
 
 //Attach middleware
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
 
 //Enter Router
-app.use('/api',  router);
+app.use("/api", router);
 
-//Start listening 
-app.listen(port, ()=>{
-  console.log('news alligator rest app started on: ' + port);
+//Start listening
+app.listen(port, () => {
+  console.log("news alligator RSS Scraper started on: " + port);
 });
 
 module.exports = app;
